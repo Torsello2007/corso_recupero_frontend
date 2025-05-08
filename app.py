3452dfb1f7870e4aa482a8ce25438cc93f9a3947
Matteo Torsello <torsello.matteo@iisgalvanimi.edu.it>
2025-05-08 13:23:00 +0000

    corso_recupero_frontend

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask, request, redirect, render_template 
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
-lista = [1, 2, 3, 4]
+studenti = [
+    {"id": 1, "nome": "Anna"},
+    {"id": 2, "nome": "Marco"}
+]
 
 @app.route("/")
 def index():
-    return render_template("index.html", lista = lista)
+    return render_template("index.html")
+
+@app.route("/aggiungi", methods=["POST"])
+def aggiungi():
+    nome = request.form["nome"]
+    nuovo = {
+        "id": len(studenti) + 1,
+        "nome": nome
+    }
+    studenti.append(nuovo)
+    return redirect("/studenti") 
+
+@app.route("/studenti")
+def elenco_studenti():
+    return render_template("elenco.html", studenti=studenti)
 
 if __name__ == "__main__":
     app.run(debug=True)
